b434f0bac1aed95f49f40def3d16fec1c784c285
ksh <ksh146>
2020-10-16 17:58:43 +0900

    MainEx 파일 추가

---
+++ VariableEx/src/kr/co/ezen/MainEx.java
@@ -0,0 +1,29 @@
+package kr.co.ezen;
+
+public class MainEx {
+	
+	public static void main(String[] args) {
+		
+		// 자료형 변수명 = 자료형에 맞는 것
+		
+		int c = 4;
+	    String msg2 = "what";
+	    String msg3 = "where";
+	    String msg4 = "hello";
+	    long aaa = 2500000000L;
+	     
+	    System.out.println(aaa);
+	    System.out.println(msg2);
+	    System.out.println(msg3);
+	    System.out.println(msg4);
+	    System.out.println(msg3);
+	    System.out.println(aaa);
+	    System.out.println(msg2);
+	    System.out.println(msg3);
+	    System.out.println(msg2);
+	    
+	    
+	    byte zzz = -128;
+	}
+
+}
