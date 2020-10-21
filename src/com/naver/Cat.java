8840d5bb4ee1f533b09e3da8a21c8337ca7b03a9
ksh146 <72857262+ksh146@users.noreply.github.com>
2020-10-21 15:19:34 +0900

    Add files via upload

---
+++ src/com/naver/Cat.java
@@ -0,0 +1,25 @@
+package com.naver;
+
+public class Cat {
+	
+	//member º¯¼ö
+	String name = "happy";
+	String master;
+	int age;
+	String address;
+	
+	public Cat() {
+		int a = 33;
+		System.out.println(name);
+		int b = 33;
+		System.out.println(b);
+		me1();
+	}
+	
+	public void me1() {
+		
+		System.out.println(name);
+	}
+	
+	
+}
---
+++ src/com/naver/MainEx.java
@@ -0,0 +1,25 @@
+package com.naver;
+
+public class MainEx {
+	public static void main(String[] args) {
+		char c = 65;
+		System.out.println(c);
+		System.out.println("==========");
+		boolean b = false;
+		System.out.println(b);
+		System.out.println("==========");
+		long longVa = 3000000000L;
+		System.out.println(longVa);
+		
+		Dog dog = new Dog();
+		
+		System.out.println("==========");
+		
+		int i = 56;
+		System.out.println(i);
+		
+		System.out.println("##########");
+		Test test = new Test();
+		
+	}
+}
